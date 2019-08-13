c3ad10a16a4c72ba01d739023064028d99530210
OVER-NULL <53993161+OVER-NULL@users.noreply.github.com>
2019-08-12 19:48:48 -0700

    Source File

---
+++ Linux/src/chars_stats.hpp
@@ -0,0 +1,21 @@
+// Version: 0.2b.0a
+// Producer: Qubit Games 
+// Date Started:  06 | 27 | 19
+// Date Finished: 10 | 04 | 19
+// Programmers: Nico M
+
+class enemy
+{
+    private:
+        
+    public:
+    
+};
+
+class ally
+{
+    private:
+        
+    public:
+    
+};
