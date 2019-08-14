80a4cca879dbd39c5f2681e72a8797033bb16e5a
OVER-NULL <53993161+OVER-NULL@users.noreply.github.com>
2019-08-14 00:47:08 -0700

    Update run.sh

--- Linux/src/run.sh
+++ Linux/src/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This file will run the make file compiling the C++ 
+# So you just type ./[insert Output File name(s)]
 # Make sure to set file permissions to executable 
 
 make -f ascii.mk 
