1ff112301d0927c9dd1c8aef01fca280d5729d5c
kevinly2893 <52688777+kevinly2893@users.noreply.github.com>
2019-08-16 17:26:21 -0700

    Add files via upload
    Update the log in and instruction page

--- Windows/src/flea_start_page.py
+++ Windows/src/flea_start_page.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import input1 as input
 
 #Illustration attempt ,(No real build according to plan)
 
@@ -13,14 +14,18 @@ fleas="""
 root = tk.Tk()
 
 root.title("Flea py Template")
+
 root.geometry("900x675") # the size of the app to be X:Y
 root.resizable(0, 0) #Don't allow resizing in the x or y direction
 root["background"]="#5d0963"
 root.label=tk.Label(root, text= fleas, font= ("Courier New", 26), fg= "white", bg = root["background"] , justify=tk.LEFT).place(relx=0.5, rely=0.15, anchor=tk.CENTER)
 
 def callback():
-    print ("click!")
+    root.destroy()
+    input.login_screen()
 
-b = tk.Button(root, text="Start",font= ("Courier New", 40),bg="Green", command=callback).place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+b = tk.Button(root, text="Press Here To Continous",font= ("Courier New", 40),bg="Green", command=callback).place(relx=0.5, rely=0.8, anchor=tk.CENTER)
 
 root.mainloop()
+
+
