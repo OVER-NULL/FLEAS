d0cf3baa85e94c78703f0cab3879fe9b1b1fd935
OVER-NULL <53993161+OVER-NULL@users.noreply.github.com>
2019-08-17 01:59:31 -0700

    Update run.sh

--- Linux/src/run.sh
+++ Linux/src/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
 # Make sure permissions are set to run as executable 
+# chmod +x filename.sh (Use to change to elf if you don't know how to set file permissions) 
+
+# Then type ./run.sh into the same directory as all other files and run 
 
 make -f ascii.mk 
